ef85439a31c1755b7716866642be1a1fc91e0c2a
Sergei Kalikin <sskalikin@gmail.com>
2025-08-22 23:54:04 +0500

    feat: updated phrase

--- greeting/hello.go
+++ greeting/hello.go
@@ -4,7 +4,7 @@ import (
 	"github.com/fatih/color"
 )
 
-var greeting string = color.BlueString("Golang") + " for " + color.RedString("Brave") + "!"
+var greeting string = color.BlueString("Golang") + " for the " + color.RedString("Brave") + " and " + color.GreenString("Skillful") + "!"
 
 func Hello() string {
 	return greeting
